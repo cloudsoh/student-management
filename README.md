aabe91aceb1fa64bb3eaf22d1c3c95e4b71392fd
Cloud <cloudsoh03021@gmail.com>
2019-07-13 14:02:18 +0800

    Environment setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # student-management
+
+
+1. `npm install`
+2. `cp .env.example .env`
+3. Fill in database info in `.env`
---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOSTNAME,
+      dialect: 'mysql',
+    },
+    test: {
+      dialect: "sqlite",
+      storage: ":memory:"
+    },
+    production: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOSTNAME,
+      dialect: 'mysql',
+    }
+  };
